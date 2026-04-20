fde5520c342066377f48d11c7ab7f1fa11bbd232
aayushi <amarrockzz9@gmail.com>
2026-04-20 09:33:28 +0530

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/react-cicd5/',
+  base: '/react-cicd5-frontend/',
 })
